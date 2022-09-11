3a4422c592bf0eda57abca23d2bb196509aacba3
Mezzky <ryansahadha@gmail.com>
2022-09-11 11:15:08 +0800

    Add: Popup Login & Register

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 const header = document.querySelector('header');
 const navToggle = document.querySelector('header .menu-toggle');
 const navbar = document.querySelector('header .navbar');
+const loginBtn = document.querySelector('#login');
+const registerBtn = document.querySelector('#register');
+const overlay = document.querySelector('.overlay');
+const loginPopup = document.querySelector('.overlay #loginBox');
+const registerPopup = document.querySelector('.overlay #registerBox');
+const loginLink = document.querySelector('.overlay .popup #loginLink');
+const registerLink = document.querySelector('.overlay .popup #registerLink');
+const popup = document.querySelectorAll('.overlay .popup');
+const close = document.querySelectorAll('.overlay .popup i');
 
 // Navbar Scroll
 window.addEventListener('scroll', () =>{
@@ -17,4 +26,38 @@ window.addEventListener('scroll', () =>{
 // Navbar Menu
 navToggle.addEventListener('click', () =>{
     navbar.classList.toggle('slide');
+});
+
+// Popup Login & Register
+loginBtn.addEventListener('click', () => {
+    loginPopup.classList.add('loginPop');
+    overlay.classList.add('loginPop');
+    document.body.classList.add('loginPop');
+});
+
+registerBtn.addEventListener('click', () => {
+    registerPopup.classList.add('regisPop');
+    overlay.classList.add('regisPop');
+    document.body.classList.add('regisPop');
+});
+
+loginLink.addEventListener('click', () => {
+    loginPopup.classList.add('loginPop');
+    registerPopup.classList.remove('regisPop');
+});
+
+registerLink.addEventListener('click', () => {
+    registerPopup.classList.add('regisPop');
+    loginPopup.classList.remove('loginPop');
+});
+
+close.forEach(el => {
+    el.addEventListener('click', () => {
+        registerPopup.classList.remove('regisPop');
+        loginPopup.classList.remove('loginPop');
+        overlay.classList.remove('loginPop');
+        overlay.classList.remove('regisPop');
+        document.body.classList.remove('regisPop');
+        document.body.classList.remove('loginPop');
+    });
 });
