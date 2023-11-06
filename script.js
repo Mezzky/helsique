450739aba3837f8f71aa28df0ec58345b00ada14
Rizky Ryan Sahadha <91834119+Mezzky@users.noreply.github.com>
2023-11-06 23:26:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$ ( d o cument).ready(() => {
+$ ( d o c ument).ready(() => {
     $(window).scroll(() => {
         if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
             $('header').addClass('scrolled');
